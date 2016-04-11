c729fa11f38146a920997a5b29f2ab1c52376ef3
Junghoon Lim <limjung0515@naver.com>
2016-04-11 17:15:51 +0900

    곱셈 기능과 테스트코드 추가
    기본기능

--- src/test/java/simcalc/SimpleCalculatorTest.java
+++ src/test/java/simcalc/SimpleCalculatorTest.java
@@ -7,10 +7,17 @@ import org.junit.Test;
 public class SimpleCalculatorTest {
 
 	@Test
-	public void testadd() {
+	public void testSub() {
 		SimpleCalculator calc = new SimpleCalculator();
 		calc.sub(10, 20);
 		assertEquals(-10, calc.getResult());
 	}
+	@Test
+	public void testMulti() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.multi(10, 20);
+		assertEquals(200, calc.getResult());
+	}
+
 
 }
